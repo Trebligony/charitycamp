b23ae901d9e93b38c0de30dbca3692896b108eb9
Test1 1 <williamh2461@gmail.com>
2022-12-01 19:59:01 -0500

    began login form function

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -4,3 +4,11 @@ import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/react-hooks";
 import { LOGIN_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
+
+const LoginForm = () => {
+    const [userFormData, setUserFormData] =useState({ email: "", password: ""});
+    const [validation] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const [login, { error }] = useMutation(LOGIN_USER);
+    
+}
