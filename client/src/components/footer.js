de3fc073c3138bca0fa383fd4292c1f53f765344
Test1 1 <williamh2461@gmail.com>
2022-12-01 19:35:41 -0500

    added the footer  to footer file

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,1 +1,17 @@
-...
+import React, { useState } from "react";
+
+const Footer = () => {
+
+    return (
+        <div class="container" >
+            <footer class="py-3 my-4">
+                <ul class="nav justify-content-center border-bottom pb-3 mb-3"></ul>
+                <p class="text-center text-muted">All Rights Reserved &copy; Non-Profit Crowdfunding - 2022 </p>
+            </footer>
+
+        </div>
+    );
+
+}
+
+export default Footer;
