6fb77cf2d02db7798368f043fa6f42d259124c4e
Test1 1 <williamh2461@gmail.com>
2022-12-02 12:52:20 -0500

    working on signup form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -5,3 +5,58 @@ import { useMutation } from "@apollo/react-hooks";
 import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 
+const SignupForm = () => {
+
+
+    const [userFormData, setUserFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+      });
+      // set state for form validation
+      const [validated] = useState(false);
+      // set state for alert
+      const [showAlert, setShowAlert] = useState(false);
+    
+      const [addUser, { error }] = useMutation(ADD_USER);
+    
+      useEffect(() => {
+        if (error) {
+          setShowAlert(true);
+        } else {
+          setShowAlert(false);
+        }
+      }, [error]);
+    
+      const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({ ...userFormData, [name]: value });
+      };
+    
+      const handleFormSubmit = async (event) => {
+        event.preventDefault();
+    
+        // check if form has everything (as per react-bootstrap docs)
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+    
+        try {
+          const { data } = await addUser({
+            variables: { ...userFormData },
+          });
+          console.log(data);
+          Auth.login(data.addUser.token);
+        } catch (err) {
+          console.error(err);
+        }
+    
+        setUserFormData({
+          username: "",
+          email: "",
+          password: "",
+        });
+      };
+}
