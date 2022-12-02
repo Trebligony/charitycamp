7fd5cd18bdcb50afda7280b9ec4b1c29a1fc48ad
Test1 1 <williamh2461@gmail.com>
2022-12-01 21:11:27 -0500

    imported all necessary items

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,6 @@
+import React, { useSate } from 'react';
+import { Link } from "react-router-dom";
+import { NavBar, Nav, Container, Model, Tab, Button } from "react-bootstrap";
+import SignUpForm from "./SignupForm";
+import LoginForm from "./LoginForm";
+import Auth from "../utils/auth";
