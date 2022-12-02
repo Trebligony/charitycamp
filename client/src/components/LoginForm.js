ab2fc1ebefc9a6f55ba7a074ac6b00b0a38f968b
Test1 1 <williamh2461@gmail.com>
2022-12-01 20:06:02 -0500

    added handleinputchange

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -10,5 +10,17 @@ const LoginForm = () => {
     const [validation] = useState(false);
     const [showAlert, setShowAlert] = useState(false);
     const [login, { error }] = useMutation(LOGIN_USER);
-    
+
+    useEffect(() => {
+        if(error) {
+            setShowAlert(true);
+        } else {
+            setShowAlert(false);
+        }
+    }, [error]);
+
+    const handleInputChange = (event) => {
+        const { name, value } =event.target;
+        setUserFormData({...userFormData, [name]: value });
+    };
 }
