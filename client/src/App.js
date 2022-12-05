7653d68a323cffd120a39631936889d6a8bcdd82
Ala Owerri <irete@alaowerri.com>
2022-12-05 18:59:43 -0500

    App.js index.js index.css file update to client/srs folder

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchProjects from "./pages/SearchProjects";
+import SavedProjects from "./pages/SavedProjects";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchProjects} />
+            <Route exact path="/saved" component={SavedProjects} />
+            <Route render={() => <h1 className="display-2">404: Page not found</h1>} />
+          </Switch>
+          <Footer/>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
