39cbaf4db09f2102c62193907ec9a43b4f4fab3f
Ala Owerri <irete@alaowerri.com>
2022-12-05 19:04:07 -0500

    Footer index html update

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
 
 const Footer = () => {
-
     return (
         <div class="container" >
             <footer class="py-3 my-4">
                 <ul class="nav justify-content-center border-bottom pb-3 mb-3"></ul>
-                <p class="text-center text-muted">All Rights Reserved &copy; Non-Profit Crowdfunding - 2022 </p>
+                <p class="text-center text-muted">All Rights Reserved &copy; Charity Camp  </p>
             </footer>
-
         </div>
     );
-
 }
 
 export default Footer;
