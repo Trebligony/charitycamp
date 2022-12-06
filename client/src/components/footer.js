ec493a2b2af58e7d8dae0ddc3f73eb7ba1d59812
Ala Owerri <irete@alaowerri.com>
2022-12-06 17:22:06 -0500

    updating App.js file src folder

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Footer = () => {
     return (
-        <div class="container" >
+        <div class="container-fluid" >
             <footer class="py-3 my-4">
                 <ul class="nav justify-content-center border-bottom pb-3 mb-3"></ul>
-                <p class="text-center text-muted">All Rights Reserved &copy; Charity Camp  </p>
+                <p class="text-center text-muted">All Rights Reserved &copy;  Charity Camp | Group 1 Project | Rutgers Coding Academy - Class of 2022 </p>
             </footer>
         </div>
     );
