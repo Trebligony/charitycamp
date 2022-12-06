5ffee228fe49014fb715bf0902d2c7672a151313
Ala Owerri <irete@alaowerri.com>
2022-12-06 06:06:34 -0500

    App.js update

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SearchProjects from "./pages/SearchProjects";
 import SavedProjects from "./pages/SavedProjects";
+import RecomendProjects from "./pages/RecomendProjects";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { ApolloProvider } from "@apollo/react-hooks";
@@ -28,6 +29,7 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/" component={SearchProjects} />
+            <Route exact path="/recommend" component={RecomendProjects} />
             <Route exact path="/saved" component={SavedProjects} />
             <Route render={() => <h1 className="display-2">404: Page not found</h1>} />
           </Switch>
